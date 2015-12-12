07ed0b5f9e6b3ae09c18516a4cc6a317e36b5ae6
orzzzl <zhuangzeleng19920731@gmail.com>
2015-12-11 21:10:36 -0500

    add dummy html

---
+++ preference.js
@@ -0,0 +1,101 @@
+$("#attr1").slider({
+	tooltip: 'always'
+});
+$("#attr1").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr2").slider({
+	tooltip: 'always'
+});
+$("#attr2").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+$("#attr3").slider({
+	tooltip: 'always'
+});
+$("#attr3").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr4").slider({
+	tooltip: 'always'
+});
+$("#attr4").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr5").slider({
+	tooltip: 'always'
+});
+$("#attr5").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr6").slider({
+	tooltip: 'always'
+});
+$("#attr6").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr7").slider({
+	tooltip: 'always'
+});
+$("#attr7").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+$("#attr8").slider({
+	tooltip: 'always'
+});
+$("#attr8").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr9").slider({
+	tooltip: 'always'
+});
+$("#attr9").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+$("#attr10").slider({
+	tooltip: 'always'
+});
+$("#attr10").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr11").slider({
+	tooltip: 'always'
+});
+$("#attr11").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr12").slider({
+	tooltip: 'always'
+});
+$("#attr12").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr13").slider({
+	tooltip: 'always'
+});
+$("#attr13").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
+$("#attr14").slider({
+	tooltip: 'always'
+});
+$("#attr14").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+$("#attr15").slider({
+	tooltip: 'always'
+});
+$("#attr15").on("slide", function (slideEvt) {
+    $("#eatingSliderVal").text(slideEvt.value);
+});
+
